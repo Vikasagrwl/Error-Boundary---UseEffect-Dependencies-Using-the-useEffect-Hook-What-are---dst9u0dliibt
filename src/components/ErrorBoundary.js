acd20565cc1d089d01d170c6f387f909f5918ec8
Vikasagrwl <105685053+Vikasagrwl@users.noreply.github.com>
2023-03-08 11:07:20 +0530

    Update ErrorBoundary.js

--- src/components/ErrorBoundary.js
+++ src/components/ErrorBoundary.js
@@ -1,17 +1,31 @@
 import React from "react";
-
+// import Component from "./Component";
 class ErrorBoundary extends React.Component {
- 
+  constructor(){
+    super();
+    this.state={
+      hasError:false
+    };
+  }
+
+  componentDidCatch(error, errorInfo){
+    this.setState({
+      hasError:true
+    });
+  }
 
   render() {
     if (this.state.hasError) {
       return (
         <p id="error">
-          
+          this is an error
         </p>
       );
     }
-    return <>{this.props.children}</>;
+    return <>
+    {this.props.children}
+    {/* <p>this is not done yet</p> */}
+    </>;
   }
 }
 export default ErrorBoundary;
